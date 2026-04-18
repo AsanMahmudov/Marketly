c40969d71b5b4d7972fabe8c5a4971b10da969b0
Asan Mahmudov <stu2501261019@uni-plovdiv.bg>
2026-04-18 23:48:19 +0300

    ADD: Message Service implemented

--- Core/Services/MessageService.cs
+++ Core/Services/MessageService.cs
@@ -1,23 +1,46 @@
-﻿using Marketly.Core.Interfaces;
+﻿using Marketly.Core.Common;
+using Marketly.Core.Interfaces;
+using Marketly.Core.Models;
 using Marketly.Core.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Marketly.Core.Services
 {
     public class MessageService : IMessageService
     {
-        public Task<IEnumerable<MessageInboxViewModel>> GetUserMessagesAsync(string userId)
+        private readonly IRepository repository;
+        public MessageService(IRepository _repository) => repository = _repository;
+
+        public async Task<IEnumerable<MessageInboxViewModel>> GetUserMessagesAsync(string userId)
         {
-            throw new NotImplementedException();
+            return await repository.All<Message>()
+                .Where(m => m.ReceiverId == userId || m.SenderId == userId)
+                .OrderByDescending(m => m.SentOn)
+                .Select(m => new MessageInboxViewModel
+                {
+                    Id = m.Id,
+                    SenderName = m.SenderId == userId ? "Me" : "User", 
+                    SenderId = m.SenderId,
+                    Content = m.Content,
+                    SentDate = m.SentOn,
+                    RelatedAdId = m.AdId
+                })
+                .ToListAsync();
         }
 
-        public Task SendMessageAsync(MessageFormModel model, string senderId)
+        public async Task SendMessageAsync(MessageFormModel model, string senderId)
         {
-            throw new NotImplementedException();
+            var message = new Message
+            {
+                SenderId = senderId,
+                ReceiverId = model.RecipientId, // Mapping RecipientId -> ReceiverId
+                Content = model.Content,
+                AdId = model.RelatedAdId ?? 0,
+                SentOn = DateTime.UtcNow
+            };
+
+            await repository.AddAsync(message);
+            await repository.SaveChangesAsync();
         }
     }
 }
