ba67301d4f277a3be51effda335c05b085be69ed
Asan Mahmudov <stu2501261019@uni-plovdiv.bg>
2026-04-18 23:45:39 +0300

    ADD: Category service implemeted

--- Core/Services/CategoryService.cs
+++ Core/Services/CategoryService.cs
@@ -1,33 +1,49 @@
-﻿using Marketly.Core.Interfaces;
+﻿using Marketly.Core.Common;
+using Marketly.Core.Interfaces;
+using Marketly.Core.Models;
 using Marketly.Core.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Marketly.Core.Services
 {
     public class CategoryService : ICategoryService
     {
-        public Task<IEnumerable<AdCategoryViewModel>> AllCategoriesAsync()
+        private readonly IRepository repository;
+        public CategoryService(IRepository _repository) => repository = _repository;
+
+        public async Task<IEnumerable<AdCategoryViewModel>> AllCategoriesAsync()
         {
-            throw new NotImplementedException();
+            return await repository.All<Category>()
+                .OrderBy(c => c.Name)
+                .Select(c => new AdCategoryViewModel { Id = c.Id, Name = c.Name })
+                .ToListAsync();
         }
 
-        public Task CreateAsync(string name)
+        public async Task<IEnumerable<CategoryAdminViewModel>> GetAllForAdminAsync()
         {
-            throw new NotImplementedException();
+            return await repository.All<Category>()
+                .Select(c => new CategoryAdminViewModel
+                {
+                    Id = c.Id,
+                    Name = c.Name,
+                    AdCount = c.Ads.Count()
+                }).ToListAsync();
         }
 
-        public Task DeleteAsync(int id)
+        public async Task CreateAsync(string name)
         {
-            throw new NotImplementedException();
+            await repository.AddAsync(new Category { Name = name });
+            await repository.SaveChangesAsync();
         }
 
-        public Task<IEnumerable<CategoryAdminViewModel>> GetAllForAdminAsync()
+        public async Task DeleteAsync(int id)
         {
-            throw new NotImplementedException();
+            var category = await repository.All<Category>().FirstOrDefaultAsync(c => c.Id == id);
+            if (category != null)
+            {
+                repository.Delete(category);
+                await repository.SaveChangesAsync();
+            }
         }
     }
 }
