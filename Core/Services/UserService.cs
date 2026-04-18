78e5092495cf1d1933d366dd225e768b606d003c
Asan Mahmudov <stu2501261019@uni-plovdiv.bg>
2026-04-18 23:48:38 +0300

    ADD: User Service Implemented

--- Core/Services/UserService.cs
+++ Core/Services/UserService.cs
@@ -1,33 +1,67 @@
-﻿using Marketly.Core.Interfaces;
+﻿using Marketly.Core.Common;
+using Marketly.Core.Interfaces;
+using Marketly.Core.Models;
 using Marketly.Core.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace Marketly.Core.Services
 {
     public class UserService : IUserService
     {
-        public Task AddToFavoritesAsync(string userId, int adId)
+        private readonly IRepository repository;
+        public UserService(IRepository _repository) => repository = _repository;
+
+        public async Task<UserProfileViewModel> GetUserProfileAsync(string userId)
         {
-            throw new NotImplementedException();
+            return await repository.All<ApplicationUser>()
+                .Where(u => u.Id == userId)
+                .Select(u => new UserProfileViewModel
+                {
+                    Id = u.Id,
+                    Username = u.UserName!,
+                    Email = u.Email!,
+                    UserAds = u.OwnedAds.Select(a => new AdMinifiedViewModel
+                    {
+                        Id = a.Id,
+                        Title = a.Title,
+                        Price = a.Price,
+                        ImageUrl = a.Images.Any() ? a.Images.First().Url : "/img/no-image.png"
+                    })
+                }).FirstAsync();
         }
 
-        public Task<IEnumerable<AdMinifiedViewModel>> GetFavoriteAdsAsync(string userId)
+        public async Task<IEnumerable<AdMinifiedViewModel>> GetFavoriteAdsAsync(string userId)
         {
-            throw new NotImplementedException();
+            return await repository.All<UserAd>()
+                .Where(ua => ua.UserId == userId)
+                .Select(ua => new AdMinifiedViewModel
+                {
+                    Id = ua.Ad.Id,
+                    Title = ua.Ad.Title,
+                    Price = ua.Ad.Price,
+                    ImageUrl = ua.Ad.Images.Any() ? ua.Ad.Images.First().Url : "/img/no-image.png"
+                }).ToListAsync();
         }
 
-        public Task<UserProfileViewModel> GetUserProfileAsync(string userId)
+        public async Task AddToFavoritesAsync(string userId, int adId)
         {
-            throw new NotImplementedException();
+            if (!await repository.All<UserAd>().AnyAsync(ua => ua.UserId == userId && ua.AdId == adId))
+            {
+                await repository.AddAsync(new UserAd { UserId = userId, AdId = adId });
+                await repository.SaveChangesAsync();
+            }
         }
 
-        public Task RemoveFromFavoritesAsync(string userId, int adId)
+        public async Task RemoveFromFavoritesAsync(string userId, int adId)
         {
-            throw new NotImplementedException();
+            var favorite = await repository.All<UserAd>()
+                .FirstOrDefaultAsync(ua => ua.UserId == userId && ua.AdId == adId);
+
+            if (favorite != null)
+            {
+                repository.Delete(favorite);
+                await repository.SaveChangesAsync();
+            }
         }
     }
 }
