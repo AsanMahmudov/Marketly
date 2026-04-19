16a85cce8824f0067e86f36e6e039ea646dd4b8c
Asan Mahmudov <stu2501261019@uni-plovdiv.bg>
2026-04-19 14:08:50 +0300

    ADD: User management controller made

---
+++ Marketly.Web/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using Marketly.Core.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+[Authorize]
+public class UsersController : Controller
+{
+    private readonly IUserService userService;
+    public UsersController(IUserService _userService) => userService = _userService;
+
+    [AllowAnonymous]
+    public async Task<IActionResult> Profile(string id)
+    {
+        var model = await userService.GetUserProfileAsync(id);
+        if (model == null) return NotFound();
+        return View(model);
+    }
+
+    public async Task<IActionResult> Watchlist()
+    {
+        var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        var ads = await userService.GetFavoriteAdsAsync(userId);
+        return View(ads); 
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> AddToWatchlist(int id)
+    {
+        await userService.AddToFavoritesAsync(User.FindFirstValue(ClaimTypes.NameIdentifier), id);
+        return RedirectToAction(nameof(Watchlist));
+    }
+}
