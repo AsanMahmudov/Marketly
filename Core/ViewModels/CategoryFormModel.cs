f02a09e0fca68100079824b3a747e8f9b6073358
Asan Mahmudov <stu2501261019@uni-plovdiv.bg>
2026-04-21 15:37:24 +0300

    ADD: Cleaned up constants in form models as well

--- Core/ViewModels/CategoryFormModel.cs
+++ Core/ViewModels/CategoryFormModel.cs
@@ -1,11 +1,13 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using static Marketly.Core.Common.DataValidationConstants.Category;
 
 namespace Marketly.Core.ViewModels
 {
     public class CategoryFormModel
     {
         [Required(ErrorMessage = "Category name is required.")]
-        [StringLength(50, MinimumLength = 3, ErrorMessage = "Category name must be between 3 and 50 characters.")]
+        [StringLength(NameMaxLength, MinimumLength = NameMinLength,
+            ErrorMessage = "Category name must be between {2} and {1} characters.")]
         [Display(Name = "Category Name")]
         public string Name { get; set; } = string.Empty;
     }
--- Core/ViewModels/MessageFormModel.cs
+++ Core/ViewModels/MessageFormModel.cs
@@ -1,9 +1,18 @@
-﻿namespace Marketly.Core.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+using static Marketly.Core.Common.DataValidationConstants.Message;
+
+namespace Marketly.Core.ViewModels
 {
     public class MessageFormModel
     {
+        [Required]
         public string RecipientId { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(ContentMaxLength, MinimumLength = ContentMinLength,
+            ErrorMessage = "Message must be between {2} and {1} characters.")]
         public string Content { get; set; } = string.Empty;
+
         public int? RelatedAdId { get; set; }
     }
 }
